9fc80637864955c896b2c451268d4fedccb05589
kaleb wondimu <kalebwondimu33@gmail.com>
2023-05-27 10:11:17 +0300

    adding bootstrap javascript and bootstrap css cdn

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,9 @@ if(isset($_SESSION['username'])){
 <head>
     <meta charset="UTF-8">
     <title>FriendLink</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     jfkldsjfslkajdklsjkafljd
