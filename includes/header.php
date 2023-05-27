97f0925ec32a1569c3e10fe9b839e74a17bdb711
kaleb wondimu <kalebwondimu33@gmail.com>
2023-05-27 10:38:42 +0300

    creating the top bar

--- includes/header.php
+++ includes/header.php
@@ -13,9 +13,17 @@ if(isset($_SESSION['username'])){
 <head>
     <meta charset="UTF-8">
     <title>FriendLink</title>
+    <!-- css file -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- javascript file -->
     <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
-    jfkldsjfslkajdklsjkafljd
+   <div class="top_bar">
+       <div class="logo">
+          <a href="index.php">Freind Link!</a>
+       </div>
+   </div>
+   
