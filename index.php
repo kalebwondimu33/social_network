99603fab3e11778db53b414232335353d00f94fe
kaleb wondimu <kalebwondimu33@gmail.com>
2023-05-29 10:20:00 +0300

    adding data to the user detail panel

--- index.php
+++ index.php
@@ -4,7 +4,17 @@ include("includes/header.php");
 ?>
     <div class="user_detail column">
        <a href=""><img src="<?php echo $user['profile_pic'];?>"></a>
-
+       <div class="user_details_left_right">
+        <a href="#">
+            <?php echo $user['fname']." ".$user['lname'];
+            ?>
+        </a>
+        <br>
+        <?php
+        echo "Post:".$user['num_posts']."<br>";
+        echo "Likes:".$user['num_likes'];
+        ?>
+       </div>
     </div>
 </div>
 </body>
