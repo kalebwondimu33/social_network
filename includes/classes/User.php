5694f00db72232c8b6f398be8fcdc914932b285a
kaleb wondimu <kalebwondimu33@gmail.com>
2023-05-31 21:00:26 +0300

    doing in posting status update

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -8,6 +8,15 @@ class User{
         $user_details_query=mysqli_query($con,"SELECT * FROM users WHERE username='$user'");
         $this->user=mysqli_fetch_array($user_details_query);
       }
+      public function getUsername(){
+        return $this->user['username'];
+      }
+      public function getNumPosts(){
+        $username=$this->user['username'];
+        $query=mysqli_query($this->con,"SELECT num_posts FROM users WHERE username='$username'");
+        $row=mysqli_fetch_array($query);
+        return $row['num_posts'];
+      }
       public function getFirstAndLastName(){
         $username=$this->user['username'];
         $query=mysqli_query($this->con,"SELECT fname , lname FROM users WHERE username='$username'");
@@ -15,19 +24,6 @@ class User{
         return $row['fname']." ".$row['lname'];
       }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 include("includes/header.php");
 include("includes/classes/User.php");
+include("includes/classes/Post.php");
+if(isset($_POST['post'])){
+    $post=new Post($con,$userLoggedIn);
+    $post->submitPost($_POST['post_text'],"none");
+}
 
 ?>
     <div class="user_detail column">
