52514092b7f8347907992b55a11d28d95a9d8450
kaleb wondimu <kalebwondimu33@gmail.com>
2023-05-31 19:45:38 +0300

    creating object from user class

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include("includes/header.php");
+include("includes/classes/User.php");
 
 ?>
     <div class="user_detail column">
@@ -22,6 +23,10 @@ include("includes/header.php");
             <input type="submit" name="post" id="post_button" value="Post">
             <hr>
         </form>
+        <?php
+         $user_obj=new User($con,$userLoggedIn);
+         echo $user_obj->getFirstAndLastName();
+        ?>
 
     </div>
 </div>
