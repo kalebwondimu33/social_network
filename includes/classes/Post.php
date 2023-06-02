483344560dd535df64ef067092808076cc3b97c6
kaleb wondimu <kalebwondimu33@gmail.com>
2023-06-02 22:19:48 +0300

    getting first name,last name and profile picture of the post added user and displaying the post

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -45,15 +45,18 @@ class Post{
           else{
             $user_to_obj=new User($this->con,$row['user_to']);
             $user_to_name=$user_to_obj->getFirstAndLastName();
-            $user_to = "<a href='".$row['user_to']."'>".$user_to_name."</a>";
+            $user_to = "to <a href='".$row['user_to']."'>".$user_to_name."</a>";
           }
           //check if user who posted has their accound closed"
           $added_by_obj=new User($this->con,$added_by);
           if($added_by_obj->isClosed()){
             continue;
           }
-          $user_detail_query=mysqli_query($this->con,"SELECT fname,lname,profile_pic FROM username='$added_by'");
+          $user_detail_query=mysqli_query($this->con,"SELECT fname,lname,profile_pic FROM users WHERE username='$added_by'");
           $user_row=mysqli_fetch_array($user_detail_query);
+          $first_name=$user_row['fname'];
+          $last_name=$user_row['lname'];
+          $profile_pic=$user_row['profile_pic'];
           //time frame
           $date_time_now=date("y-m-d H:i:s");
           $start_date=new DateTime($date_time);//time of post
@@ -91,6 +94,7 @@ class Post{
             else{
               $days=$interval->d." days ago";
             }
+            $time_message = $days;
 
           }
           elseif($interval->h>=1){
@@ -117,8 +121,21 @@ class Post{
               $time_message=$interval->s." second ago";
             }
           }
+          $str.="<div class='status_post'>
+                 <div class='post_profile_pic'>
+                  <img src='$profile_pic' width='50'>
+                </div>
+                <div class='posted_by' style='color:#ACACAC;'>
+                <a href='$added_by'>$first_name $last_name</a>$user_to &nbsp;&nbsp;&nbsp;&nbsp;$time_message
+                </div>
+                <div id='post_body'>
+                $body
+                <br>
+                </div>
+                </div>";
           
         }
+        echo $str;
             }
             
 }
