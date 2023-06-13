45c0257c3730e3ebdb49a6381e43b7a4d8c0c9cf
kaleb wondimu <kalebwondimu33@gmail.com>
2023-06-13 16:39:24 +0300

    styling the left profile bar

--- profile.php
+++ profile.php
@@ -8,6 +8,13 @@ if(isset($_GET['profile_username'])){
 }
 
 ?>
+<style type="text/css">
+  .wrapper{
+    margin-left: 0px;
+    padding-left: 0px;
+  }
+
+</style>
 <div class="profile_left">
   <img src="<?php echo $user_array['profile_pic']; ?>">
   <div class="profile_info">
