bc48438ba608fb69221485d1accf9c0f79765fa0
kaleb wondimu <kalebwondimu33@gmail.com>
2023-06-27 18:34:57 +0300

    delete button functionality and styling

--- includes/header.php
+++ includes/header.php
@@ -25,6 +25,7 @@ if(isset($_SESSION['username'])){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/aaa1dd84b2.js" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/6.0.0/bootbox.min.js"></script>
     <script src="assets/js/demo.js"></script>
     
 </head>
