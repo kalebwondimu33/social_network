1650f9b2f20bc819240c99fd53084c0af2c35df9
kaleb wondimu <kalebwondimu33@gmail.com>
2023-07-01 21:06:04 +0300

    finishing the retriving message part

--- includes/classes/message.php
+++ includes/classes/message.php
@@ -34,8 +34,16 @@ class Message{
         $userLoggedIn = $this->user_obj->getUsername();
         $data="";
         $query=mysqli_query($this->con,"UPDATE messages SET opened='yes' WHERE user_to='$userLoggedIn' AND user_from='$otherUser'");
-        $get_messages_query=mysqli_query($this->con,"SELECT * FROM mesages WHERE (user_to='$userLoggedIn' AND user_from='$otherUser')OR (user_from='$userLoggedIn' AND user_to='$otherUser')");
+        $get_messages_query=mysqli_query($this->con,"SELECT * FROM messages WHERE (user_to='$userLoggedIn' AND user_from='$otherUser')OR (user_from='$userLoggedIn' AND user_to='$otherUser')");
+        while($row=mysqli_fetch_array($get_messages_query)){
+          $user_to=$row['user_to'];
+          $user_from=$row['user_from'];
+          $body=$row['body'];
+          $div_top=($user_to==$userLoggedIn) ? "<div class='message' id='green'>":"<div class='message id='blue'>";
+          $data=$data . $div_top . $body ."</div><br><br>";
 
       }
+      return $data;
+    }
     }
     ?>
