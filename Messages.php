439e2d34124a4a17616860b336ac12c1472bde91
kaleb wondimu <kalebwondimu33@gmail.com>
2023-07-02 15:12:53 +0300

    new message

--- Messages.php
+++ Messages.php
@@ -57,7 +57,7 @@ if(isset($_POST['post_message'])){
             <?php
             if($user_to == "new"){
                 echo "select the friend you would like to message <br><br>";
-                echo "To: <input type='text' >";
+                echo "To: <input type='text' onkeyup='getUsers(this.value,<?php echo $userLoggedIn;?>)' name='q' placeholder='Name' autocomplete='off' id='search_text_input' >";
                 echo "<div class='results'></div>";
             }
             else{
